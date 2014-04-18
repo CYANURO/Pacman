a033fbce9174d8b2b4b2827f08b4ea7b71b71d26
doppey <aldoosamelli@gmail.com>
2014-04-18 10:13:13 -0600

    First Commit
    First commit to making the Pacman game.

---
+++ Pacman/Pacman/Game1.cs
@@ -0,0 +1,120 @@
+#region Using
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+#endregion 
+
+namespace Pacman
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+
+        #region Private Variables 
+        /// <summary>
+        /// Graphics device manager
+        /// </summary>
+        private GraphicsDeviceManager graphics;
+
+        /// <summary>
+        /// The spriteBatch
+        /// </summary>
+        private SpriteBatch spriteBatch;
+
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Game Constructor 
+        /// </summary>
+        public Game1()
+        {
+            ///Create new graphics design manager
+            graphics = new GraphicsDeviceManager(this);
+
+            ///Set the Content root directory
+            Content.RootDirectory = "Content";
+        }
+        #endregion 
+
+        #region Initialize
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+
+            base.Initialize();
+        }
+
+        #endregion 
+
+        #region LoadContent
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        }
+
+        #endregion 
+
+        #region UnloadContent
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+
+        }
+        #endregion 
+
+        #region Update
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+
+            base.Update(gameTime);
+        }
+        #endregion 
+
+        #region Draw
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+
+            base.Draw(gameTime);
+        }
+        #endregion
+    }
+}
---
+++ Pacman/Pacman/Objects.cs
@@ -0,0 +1,55 @@
+﻿#region Using
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+namespace Pacman
+{
+    class Objects
+    {
+        /// <summary>
+        /// The texture of the object
+        /// </summary>
+        public Texture2D Texture = null;
+
+        /// <summary>
+        /// The texture name of the object
+        /// </summary>
+        public string TextureName = String.Empty;
+
+        /// <summary>
+        /// The center of the texture
+        /// </summary>
+        public Vector2 Center = Vector2.Zero;
+
+        /// <summary>
+        /// The position of the object on the screen
+        /// </summary>
+        public Vector2 Position = Vector2.Zero;
+
+        public Objects(Vector2 pos)
+        {
+
+        }
+
+        public virtual void LoadContent(ContentManager content)
+        {
+
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
